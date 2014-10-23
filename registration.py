c75346a10555cae282842aa30676de58de579d59
Lars Buitinck <l.buitinck@esciencecenter.nl>
2014-10-23 15:09:11 +0200

    don't silently ignore erroneous user input in registration.py
    Explicit is better than implicit.

--- registration.py
+++ registration.py
@@ -2,9 +2,12 @@
 Run the registration algorithm on PCD or ply files files.
 """
 
+from __future__ import print_function
+
 import argparse
 import pcl
 import pcl.registration
+import sys
 
 def parse_args():
     """ Parse arguments from the command-line using argparse """
@@ -16,9 +19,9 @@ def parse_args():
 
 def process_args(args):
     """ Read source, target and algorithm """
-    print "Reading source", args.source
+    print("Reading source {}".format(args.source))
     source = pcl.load(args.source)
-    print "Reading target ", args.target
+    print("Reading target {}".format(args.target))
     target = pcl.load(args.target)
     
     funcs = {
@@ -27,13 +30,17 @@ def process_args(args):
         'icp_nl': pcl.registration.icp_nl,
         'ia_ransac': pcl.registration.ia_ransac
     }
-    
-    if args.function in funcs:
+
+    try:
         algo = funcs[args.function]
-    else:
-        algo = pcl.registration.gicp
-    
-    return source, target, algo
+    except KeyError:
+        print("Unknown algorithm {!r}".format(args.function), file=sys.stderr)
+        print("Valid algorithms are:", file=sys.stderr)
+        for algo in funcs:
+            print("    {!r}".format(algo))
+        sys.exit(1)
+
+    return source, target, funcs[args.function]
 
 def run_algo(algorithm, source, target, **kwargs):
     
@@ -41,12 +48,12 @@ def run_algo(algorithm, source, target, **kwargs):
 def print_output(algo, converged, transf, fitness):
     """ Print some output based on the algorithm output """
     
-    print "------", algo.__name__, "-----"
-    print "Converged: ", converged, "Fitness: ", fitness
-    print "Rotation: "
-    print transf[0:3,0:3]
-    print "Translation: ", transf[3, 0:3]
-    print "---------------"
+    print("------ {} -----".format(algo.__name__))
+    print("Converged: {}; Fitness = {}".format(converged, fitness))
+    print("Rotation: ")
+    print(transf[0:3,0:3])
+    print("Translation: {}".format(transf[3, 0:3]))
+    print("---------------")
 
 if __name__ == '__main__':
     args = parse_args()
