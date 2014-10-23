74c86d44d4d235b455ae1699868fd17cc740932b
Joris Borgdorff <joris@jorisborgdorff.nl>
2014-10-23 14:36:10 +0200

    Refactoring registration.py

--- registration.py
+++ registration.py
@@ -1,5 +1,5 @@
 """
-Run the registration algorithm on PCD files.
+Run the registration algorithm on PCD or ply files files.
 """
 
 import argparse
@@ -7,30 +7,39 @@ import pcl
 import pcl.registration
 
 def parse_args():
+    """ Parse arguments from the command-line using argparse """
     parser = argparse.ArgumentParser(description='Registration for two PLY point clouds')
     parser.add_argument('-f','--function', help='Registration algorithm to run. Choose between gicp, icp, icp_nl, and ia_ransac. Defaults to gicp')
-    parser.add_argument('source', metavar="SRC_PLY", help="Source PLY file")
-    parser.add_argument('target', metavar="TARGET_PLY", help="Target PLY file to map source to")
+    parser.add_argument('source', metavar="SOURCE", help="Source PLY/PCD file")
+    parser.add_argument('target', metavar="TARGET", help="Target PLY/PCD file to map source to")
     return parser.parse_args()
 
-if __name__ == '__main__':
-    args = parse_args()
-    
+def process_args(args):
+    """ Read source, target and algorithm """
+    print "Reading source", args.source
     source = pcl.load(args.source)
+    print "Reading target ", args.target
     target = pcl.load(args.target)
     
     funcs = {
         'icp': pcl.registration.icp,
         'gicp': pcl.registration.gicp,
         'icp_nl': pcl.registration.icp_nl,
-        'ia_ransac': pcl.registration.ia_ransac}
+        'ia_ransac': pcl.registration.ia_ransac
+    }
     
     if args.function in funcs:
         algo = funcs[args.function]
     else:
         algo = pcl.registration.gicp
-        
-    converged, transf, estimate, fitness = algo(source, target)
+    
+    return source, target, algo
+
+def run_algo(algorithm, source, target, **kwargs):
+    
+
+def print_output(algo, converged, transf, fitness):
+    """ Print some output based on the algorithm output """
     
     print "------", algo.__name__, "-----"
     print "Converged: ", converged, "Fitness: ", fitness
@@ -38,3 +47,9 @@ if __name__ == '__main__':
     print transf[0:3,0:3]
     print "Translation: ", transf[3, 0:3]
     print "---------------"
+
+if __name__ == '__main__':
+    args = parse_args()
+    source, target, algo = process_args(args)
+    converged, transf, estimate, fitness = algo(source, target)
+    print_output(algo, converged, transf, fitness)
