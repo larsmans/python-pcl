24dd14dcccf599c214c4ff34cb31e2b66071ae22
Joris Borgdorff <joris@jorisborgdorff.nl>
2014-10-25 13:30:14 +0200

    More modular registration script

--- registration.py
+++ registration.py
@@ -3,47 +3,39 @@ Run the registration algorithm on PCD or ply files files.
 """
 
 from __future__ import print_function
-
 import argparse
 import pcl
 import pcl.registration
-import liblas
-import numpy as np
 import time
 import sys
+import numpy as np
+import conversions
 
-def loadLas(lasFile):
-    las = liblas.file.File(lasFile)
-    nPoints = las.header.get_count()
-    data_xyz = np.zeros((nPoints, 6), dtype=np.float64)
-    min_point = np.array(las.header.get_min())
-    max_point = np.array(las.header.get_max())
-    offset = min_point + (max_point - min_point)/2
+def log(*args, **kwargs):
+    print(time.strftime("[%H:%M:%S]"), *args, **kwargs)
 
-    for i,point in enumerate(las):
-        data_xyz[i,0:3] = point.x,point.y,point.z
-        data_xyz[i,0:3] -= offset
-        data_xyz[i,3:6] = point.color.red,point.color.green,point.color.blue
-        data_xyz[i,3:6] /= 256
+def fail_process_args(parser, *args, **kwargs):
+    log(*args, **kwargs)
+    parser.print_help()
+    sys.exit(1)
 
-    pc = pcl.PointCloudXYZRGB(data_xyz.astype(np.float32))
-    return pc, offset
-
-def parse_args():
+def process_args():
     """ Parse arguments from the command-line using argparse """
     parser = argparse.ArgumentParser(description='Registration for two PLY point clouds')
-    parser.add_argument('-f','--function', help='Registration algorithm to run. Choose between gicp, icp, icp_nl, and ia_ransac. Defaults to gicp')
+    parser.add_argument('-f','--function', help='Registration algorithm to run. Choose between gicp, icp, icp_nl, and ia_ransac.', default='gicp')
+    parser.add_argument('-d','--downsample', type=float, help='Downsample to use one point per given voxel size. Suggested value: 0.005.')
     parser.add_argument('source', metavar="SOURCE", help="Source LAS file")
     parser.add_argument('target', metavar="TARGET", help="Target LAS file to map source to")
-    return parser.parse_args()
+    
+    args = parser.parse_args()
 
-def process_args(args):
-    """ Read source, target and algorithm """
-    print time.strftime("%H:%M:%S"), ": reading source", args.source
+    log("reading source", args.source)
     # source = pcl.load(args.source)
-    source, _ = loadLas(args.source)
-    print time.strftime("%H:%M:%S"), ": reading target ", args.target
-    target, _ = loadLas(args.target)
+    source, src_offset = conversions.loadLas(args.source)
+    log("offset:", src_offset)
+    log("reading target ", args.target)
+    target, tgt_offset = conversions.loadLas(args.target)
+    log("offset:", tgt_offset)
     # target = pcl.load(args.target)
     
     funcs = {
@@ -53,44 +45,63 @@ def process_args(args):
         'ia_ransac': pcl.registration.ia_ransac
     }
 
-    if args.function == None:
-        algo = pcl.registration.gicp
+    if args.function in funcs:
+        algo = funcs[args.function]
     else:
-        try:
-            algo = funcs[args.function]
-        except KeyError:
-            print("Unknown algorithm {!r}".format(args.function), file=sys.stderr)
-            print("Valid algorithms are:", file=sys.stderr)
-            for algo in funcs:
-                print("    {!r}".format(algo))
-            sys.exit(1)
-
-    return source, target, algo
+        fail_process_args(parser, "unknown algorithm", args.function)
+    
+    if args.downsample is not None and (args.downsample >= 1.0 or args.downsample < 0.0):
+        fail_process_args("choose downsampling size between 0 and 1 -- suggested: 0.005")
+        
+    return source, target, algo, args.downsample
 
 def print_output(algo, converged, transf, fitness):
     """ Print some output based on the algorithm output """
     
-    print "Converged: ", converged, "Fitness: ", fitness
-    print "Rotation: "
-    print transf[0:3,0:3]
-    print "Translation: ", transf[3, 0:3]
-    print time.strftime("%H:%M:%S"), ": ---------------"
+    log("converged:", converged, "- error:", fitness)
+    log("rotation:")
+    log("\n", transf[0:3,0:3])
+    log("translation:", transf[3, 0:3])
+    log("---------------")
+
+def length_3d(pointcloud):
+    xyz_array = pointcloud.to_array()[:,0:3]
+    return xyz_array.max(axis=0) - xyz_array.min(axis=0)
+
+def scale(pointcloud, scale_factor):
+    transf = np.identity(4, dtype=float)*scale_factor
+    transf[3,3] = 1.0
+    return pointcloud.transform(transf)
+
+def downsample(pointcloud, voxel_size=0.01):
+    old_len = len(pointcloud)
+    pc_filter = source.make_voxel_grid_filter()
+    pc_filter.set_leaf_size(voxel_size, voxel_size, voxel_size)
+    filtered_pointcloud = pc_filter.filter()
+    new_len = len(filtered_pointcloud)
+    decrease_percent = (old_len - new_len)*100 / old_len
+    log("number of points reduced from", old_len, "to", new_len, "(", decrease_percent, "% decrease)")
+    return filtered_pointcloud
 
 if __name__ == '__main__':
-    args = parse_args()
-    source, target, algo = process_args(args)
+    source, target, algo, voxel_size = process_args()
+    
+    # choose the maximum size over all coordinates to determine scale
+    src_maxsize = max(length_3d(source))
+    tgt_maxsize = max(length_3d(target))
     
     # preprocess source and target
-    print "Filtering source point cloud to leaf size 0.01"
-    source_filter = source.make_voxel_grid_filter()
-    source_filter.set_leaf_size(0.01, 0.01, 0.01)
-    source = source_filter.filter()
+    log("scaling source down by: ", src_maxsize)
+    source = scale(source, 1.0/src_maxsize)
+    log("scaling target down by: ", tgt_maxsize)
+    target = scale(target, 1.0/tgt_maxsize)
 
-    print "Filtering target point cloud to leaf size 0.01"
-    target_filter = target.make_voxel_grid_filter()
-    target_filter.set_leaf_size(0.01, 0.01, 0.01)
-    target = target_filter.filter()
-    
-    print time.strftime("%H:%M:%S"), ": ------", algo.__name__, "-----"
+    if voxel_size is not None:
+        log("downsampling source using voxel size", voxel_size)
+        source = downsample(source, voxel_size=voxel_size)
+        log("downsampling target using voxel size", voxel_size)
+        target = downsample(target, voxel_size=voxel_size)
+
+    log("------", algo.__name__, "-----")
     converged, transf, estimate, fitness = algo(source, target)
     print_output(algo, converged, transf, fitness)
